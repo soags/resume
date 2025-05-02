2b5491c2bfd8cd6400788f8695b106b4dfd89e73
soags <17847282+soags@users.noreply.github.com>
2025-05-03 06:18:12 +0900

    デプロイ設定

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,7 +1,13 @@
 import type { Config } from "@react-router/dev/config";
+import { copyFile } from "fs/promises";
+import path from "path";
 
 export default {
-  async prerender() {
-    return ["/"]
+  basename: import.meta.env.PROD ? '/resume/' : '/',
+  async buildEnd(args): Promise<void> {
+    if (!args.viteConfig.isProduction) return;
+    const buildPath = args.viteConfig.build.outDir;
+    await copyFile(path.join(buildPath, 'index.html'), path.join(buildPath, '404.html'));
   },
+  ssr: false,
 } satisfies Config;

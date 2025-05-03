f5dccd0f42df939db7c50aa17bc5eb6a5987c774
soags <17847282+soags@users.noreply.github.com>
2025-05-04 03:41:47 +0900

    PDFファイル名を固定

--- generate-pdf.js
+++ generate-pdf.js
@@ -1,28 +1,17 @@
 import puppeteer from 'puppeteer'
-;(async () => {
-  const URL = 'https://soags.github.io/resume/'
-  const FILE_NAME = `職務経歴書_${getDateStamp()}.pdf`
 
-  const browser = await puppeteer.launch({
-    headless: 'new',
-  })
+(async () => {
+  const URL = 'https://soags.github.io/resume/'
+
+  const browser = await puppeteer.launch({ headless: 'new' })
   const page = await browser.newPage()
   await page.goto(URL, { waitUntil: 'networkidle0' })
 
   await page.pdf({
-    path: FILE_NAME,
+    path: '職務経歴書.pdf',
     format: 'A4',
     printBackground: true,
   })
 
   await browser.close()
 })()
-
-function getDateStamp() {
-  const today = new Date()
-  const year = today.getFullYear()
-  const month = String(today.getMonth() + 1).padStart(2, '0')
-  const day = String(today.getDate()).padStart(2, '0')
-
-  return `${year}${month}${day}`
-}
